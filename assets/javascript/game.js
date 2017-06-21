cebfc10b02499884433d3773efccb68dd8617976
Cody McCoy <mccoycody67@gmail.com>
2017-06-20 21:19:06 -0500

    Outline

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+(function () {
+  var availableLetters
+  var words
+      function setup() {
+        availableLetters = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+        lives = 5;
+        words = ["cowboys, giants, eagles, redskins"];
+          messages = {
+            win: "Touchdown!";
+            lose: "Better luck next week...";
+          };
+      }
+})
