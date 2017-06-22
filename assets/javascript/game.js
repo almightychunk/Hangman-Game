e78f244adf8596cc6db2fd77280c3b01761de77f
Cody McCoy <Cody McCoy>
2017-06-22 16:10:58 -0500

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,10 @@
-(function () {
-  var availableLetters
-  var words
-      function setup() {
-        availableLetters = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-        lives = 5;
-        words = ["cowboys, giants, eagles, redskins"];
-          messages = {
-            win: "Touchdown!";
-            lose: "Better luck next week...";
-          };
-      }
-})
+var availableLetters = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
+
+  var options = ["cowboys","giants", "eagles", "redskins"];
+  function randWord() {
+      var random = options[Math.floor(Math.random()*options.length)];
+      console.log(random);
+      var targetDiv = document.getElementById("randWord");
+      targetDiv.innerHTML = random;
+  };
+randWord();
